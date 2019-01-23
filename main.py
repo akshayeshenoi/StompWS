a4392021f49cbbe778dae1c50d2fef1587408447
Akshaye Shenoi <akshaye.shenoi@gmail.com>
2019-01-23 15:25:21 +0530

    added subscribe and send functionality

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from stomp_ws import Stomp
+import time
+
+def do_thing_a(msg):
+    print("MESSAGE: " + msg)
 
 if __name__ == "__main__":
     stomp = Stomp("127.0.0.1:8080/gs-guide-websocket", sockjs=True, wss=False)
     stomp.connect()
+    stomp.subscribe("/topic/greetings", do_thing_a)
+    time.sleep(2)
+    stomp.send("/app/hello", '{"name":"akshaye"}')
--- stomp_ws/stomp.py
+++ stomp_ws/stomp.py
@@ -19,13 +19,16 @@ class Stomp:
             sockjs: True if the STOMP server is sockjs
             wss: True if communication is over SSL
         """
-        websocket.enableTrace(True)
+        # websocket.enableTrace(True)
         ws_host = host if sockjs is False else host + "/websocket"
         protocol = "ws://" if wss is False else "wss://"
 
         self.url = protocol + ws_host
 
         self.dispatcher = Dispatcher(self)
+
+        # maintain callback registry for subscriptions -> topic (str) vs callback (func)
+        self.callback_registry = {}
 
     def connect(self):
         """
@@ -42,6 +45,20 @@ class Stomp:
             time.sleep(.50)
 
         return self.connected
+
+    def subscribe(self, destination, callback):
+        """
+        Subscribe to a destination and supply a callback that should be executed when a message is received on that destination
+        """
+        # create entry in registry against destination
+        self.callback_registry[destination] = callback
+        self.dispatcher.subscribe(destination)
+
+    def send(self, destination, message):
+        """
+        Send a message to a destination
+        """
+        self.dispatcher.send(destination, message)
 
 
 class Dispatcher:
@@ -74,10 +91,15 @@ class Dispatcher:
         """
         print("<<< " + message)
 
-        command = self._getCommand(message)
+        command, headers, body = self._parse_message(message)
 
-        if command.strip() == "CONNECTED":
+        # if connected, let Stomp know
+        if command == "CONNECTED":
             self.stomp.connected = True
+
+        # if message received, call appropriate callback
+        if command == "MESSAGE":
+            self.stomp.callback_registry[headers['destination']](body)
 
     def _on_error(self, ws, error):
         """
@@ -97,31 +119,60 @@ class Dispatcher:
         """
         self.opened = True
 
-    def _transmit(self, command, headers):
+    def _transmit(self, command, headers, msg=None):
         """
-        Marshalls and transmits the marshalled frame
+        Marshalls and transmits the frame
         """
         # Contruct the frame
         lines = []
         lines.append(command + BYTE['LF'])
 
+        # add headers
         for key in headers:
             lines.append(key + ":" + headers[key] + BYTE['LF'])
 
-        frame = ''.join(lines) + BYTE['LF'] + BYTE['NULL']
+        lines.append(BYTE['LF'])
+
+        # add message, if any
+        if msg is not None:
+            lines.append(msg)
+
+        # terminate with null octet
+        lines.append(BYTE['NULL'])
+    
+        frame = ''.join(lines)
 
         # transmit over ws
         print(">>>" + frame)
         self.ws.send(frame)
 
-    def _getCommand(self, frame):
+    def _parse_message(self, frame):
         """
-        Returns frame command
+        Returns:
+            command
+            headers
+            body
 
         Args:
             frame: raw frame string
         """
-        return frame.split(BYTE['LF'])[0]
+        lines = frame.split(BYTE['LF'])
+
+        command = lines[0].strip()
+        headers = {}
+
+        # get all headers
+        i = 1
+        while lines[i] != '':
+            # get key, value from raw header
+            (key, value) = lines[i].split(':')
+            headers[key] = value
+            i += 1
+
+        # set body to None if there is no body
+        body = None if lines[i+1] == BYTE['NULL'] else lines[i+1]
+
+        return command, headers, body
 
     def connect(self):
         """
@@ -134,3 +185,27 @@ class Dispatcher:
         headers['heart-beat'] = '10000,10000'
 
         self._transmit('CONNECT', headers)
+
+    def subscribe(self, destination):
+        """
+        Transmit a SUBSCRIBE frame
+        """
+        headers = {}
+
+        # TODO id should be auto generated
+        headers['id'] = 'sub-1'
+        headers['ack'] = 'client'
+        headers['destination'] = destination
+
+        self._transmit('SUBSCRIBE', headers)
+
+    def send(self, destination, message):
+        """
+        Transmit a SEND frame
+        """
+        headers = {}
+
+        headers['destination'] = destination
+        headers['content-length'] = str(len(message))
+
+        self._transmit('SEND', headers, msg=message)
